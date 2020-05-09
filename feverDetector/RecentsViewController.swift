76652c46ff5a91767b549a24c6d8710ded507c51
hayaton <hayaton@bu.edu>
2020-05-08 22:50:33 -0400

    new

--- feverDetector/RecentsViewController.swift
+++ feverDetector/RecentsViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import Charts
 
 // MARK: - Recents
 struct Recents: Codable {
@@ -42,6 +43,7 @@ class RecentsViewController: UIViewController {
     @IBOutlet weak var bodyTempLabel: UILabel!
     @IBOutlet weak var outsideBodyTempLabel: UILabel!
     @IBOutlet weak var confidenceLabel: UILabel!
+    @IBOutlet weak var feverChart: HorizontalBarChartView!
     
     var pictureName = [] as [String]
     var bodyTemp = [] as [String]
@@ -124,11 +126,13 @@ class RecentsViewController: UIViewController {
                     self.bodyTemp.append(ten.mean)
                     self.weather.append(ten.weather)
                     self.name.append(ten.name.rawValue)
-                                        
+                    
+                    
+                    
                     print(self.pictureName)
                     self.setImage(from: self.pictureName[0])
                     self.updateLabel(num: self.counter)
-                    
+                    //self.update_fever_bar()
                     
 
                   } catch let error {
@@ -138,7 +142,7 @@ class RecentsViewController: UIViewController {
            }.resume()
         }
         
-        
+    
     }
     
     
@@ -153,9 +157,12 @@ class RecentsViewController: UIViewController {
             
             self.nameLabel.text = "Name: " + self.name[num]
             self.dateLabel.text = "Date: " + String(newstring)
-            self.bodyTempLabel.text = "Body Temp: "+self.bodyTemp[num] + "°C"
-            self.outsideBodyTempLabel.text = "Weather: " + self.weather[num] + "°C"
+            //self.bodyTempLabel.text = "Body Temp: "+self.bodyTemp[num] + "°C"
+            //self.outsideBodyTempLabel.text = "Weather: " + self.weather[num] + "°C"
+            
+            //self.update_fever_bar()
         }
+        update_fever_bar()
     }
     
     
@@ -163,7 +170,7 @@ class RecentsViewController: UIViewController {
         let handlerone = "https://hayatopia.s3.amazonaws.com/"
         let handlertwo = "https://facescolumbia.s3.amazonaws.com/"
         let heatmapURL = handlerone + url + "_HEATMAP.png"
-        let faceURL = handlertwo + url + ".jpg_cropped.jpg" //CHANGE
+        let faceURL = handlertwo + url + "_cropped.jpg" //CHANGE
         
         guard let heatImageURL = URL(string: heatmapURL) else { return }
 
@@ -191,14 +198,171 @@ class RecentsViewController: UIViewController {
     
     @IBAction func rightButton(_ sender: Any) {
         self.counter += 1
-        let now = (self.counter)%10
+        let now = abs((self.counter)%10)
         setImage(from: self.pictureName[now])
         pictureNumber.text = String(now+1) + "/10"
         
         self.setImage(from: self.pictureName[now])
+        update_fever_bar()
         updateLabel(num: now)
         
     }
+    
+    @IBAction func leftButton(_ sender: Any) {
+        self.counter -= 1
+        let now = abs((self.counter)%10)
+        setImage(from: self.pictureName[now])
+        pictureNumber.text = String(now+1) + "/10"
+        
+        self.setImage(from: self.pictureName[now])
+        update_fever_bar()
+        updateLabel(num: now)
+    }
+    
+    @IBAction func refreshButton(_ sender: Any) {
+        
+        if let url = URL(string: "https://hayatopia.s3.amazonaws.com/recent.json") {
+           URLSession.shared.dataTask(with: url) { data, response, error in
+              if let data = data {
+                  do {
+                     let data = try JSONDecoder().decode(Recents.self, from: data)
+                    self.counter = 0
+                    self.pictureName = [] as [String]
+                    self.bodyTemp = [] as [String]
+                    self.weather = [] as [String]
+                    self.name = [] as [String]
+                    
+                    let one = data.one[0]
+                    self.pictureName.append(one.pictureName)
+                    self.bodyTemp.append(one.mean)
+                    self.weather.append(one.weather)
+                    self.name.append(one.name.rawValue)
+                    
+                    let two = data.two[0]
+                    self.pictureName.append(two.pictureName)
+                    self.bodyTemp.append(two.mean)
+                    self.weather.append(two.weather)
+                    self.name.append(two.name.rawValue)
+                    
+                    let three = data.three[0]
+                    self.pictureName.append(three.pictureName)
+                    self.bodyTemp.append(three.mean)
+                    self.weather.append(three.weather)
+                    self.name.append(three.name.rawValue)
+                    
+                    let four = data.four[0]
+                    self.pictureName.append(four.pictureName)
+                    self.bodyTemp.append(four.mean)
+                    self.weather.append(four.weather)
+                    self.name.append(four.name.rawValue)
+                    
+                    let five = data.five[0]
+                    self.pictureName.append(five.pictureName)
+                    self.bodyTemp.append(five.mean)
+                    self.weather.append(five.weather)
+                    self.name.append(five.name.rawValue)
+                    
+                    let six = data.six[0]
+                    self.pictureName.append(six.pictureName)
+                    self.bodyTemp.append(six.mean)
+                    self.weather.append(six.weather)
+                    self.name.append(six.name.rawValue)
+                    
+                    let seven = data.seven[0]
+                    self.pictureName.append(seven.pictureName)
+                    self.bodyTemp.append(seven.mean)
+                    self.weather.append(seven.weather)
+                    self.name.append(seven.name.rawValue)
+                    
+                    let eight = data.eight[0]
+                    self.pictureName.append(eight.pictureName)
+                    self.bodyTemp.append(eight.mean)
+                    self.weather.append(eight.weather)
+                    self.name.append(eight.name.rawValue)
+                    
+                    let nine = data.nine[0]
+                    self.pictureName.append(nine.pictureName)
+                    self.bodyTemp.append(nine.mean)
+                    self.weather.append(nine.weather)
+                    self.name.append(nine.name.rawValue)
+                    
+                    let ten = data.ten[0]
+                    self.pictureName.append(ten.pictureName)
+                    self.bodyTemp.append(ten.mean)
+                    self.weather.append(ten.weather)
+                    self.name.append(ten.name.rawValue)
+                                        
+                    print(self.pictureName)
+                    self.setImage(from: self.pictureName[0])
+                    self.updateLabel(num: self.counter)
+                    //self.update_fever_bar()
+                    
+
+                  } catch let error {
+                     print(error)
+                  }
+               }
+           }.resume()
+        }
+    }
+    
+    
+    
+    func update_fever_bar() {
+        
+        let maxTemp = 42.0
+        let index = (self.counter)%10
+        let currTemp = bodyTemp[index]
+        self.feverChart.drawBarShadowEnabled = true
+        self.feverChart.drawValueAboveBarEnabled = true
+        self.feverChart.maxVisibleCount = Int(maxTemp)
+        self.feverChart.rightAxis.axisMaximum = maxTemp
+        self.feverChart.leftAxis.axisMaximum = maxTemp
+                
+        let xAxis  = self.feverChart.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = false
+        xAxis.granularity = 10.0
+        
+        let leftAxis = self.feverChart.leftAxis;
+        leftAxis.drawAxisLineEnabled = false;
+        leftAxis.drawGridLinesEnabled = true;
+        leftAxis.axisMinimum = 30.0; // this replaces startAtZero = YES
+        leftAxis.enabled = false
+        
+        let rightAxis = self.feverChart.rightAxis
+        rightAxis.enabled = true;
+        rightAxis.drawAxisLineEnabled = false;
+        rightAxis.drawGridLinesEnabled = true;
+        rightAxis.axisMinimum = 30.0; // this replaces startAtZero = YES
+        
+        let l = self.feverChart.legend
+        l.enabled =  false
+        feverChart.fitBars = true;
+        self.feverChart.animate(xAxisDuration: 2, yAxisDuration: 2)
+        
+        let barWidth = 0.6
+        var yVals = [BarChartDataEntry]()
+        yVals.append(BarChartDataEntry(x: Double(1.0), y: Double(currTemp)!))
+        var feverVal = [BarChartDataEntry]()
+        yVals.append(BarChartDataEntry(x: Double(1.0), y: Double(38.0)))
+        
+        var set1 : BarChartDataSet!
+        set1 = BarChartDataSet(entries: yVals, label: "DataSet")
+        set1.colors = [#colorLiteral(red: 1, green: 0, blue: 0.3303074837, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)]
+        //var set2: BarChartDataSet!
+        //set2 = BarChartDataSet(entries: feverVal, label: "Fever Val")
+        //set2.colors = [#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)]
+        var dataSets = [BarChartDataSet]()
+        dataSets.append(set1)
+        //dataSets.append(set2)
+        let data = BarChartData(dataSets: dataSets)
+        data.barWidth =  barWidth;
+        feverChart.data = data
+        //feverChart.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.3303074837, alpha: 1)
+    }
+    
     
     /*
     // MARK: - Navigation
--- feverDetector/ViewController.swift
+++ feverDetector/ViewController.swift
@@ -7,14 +7,105 @@
 //
 
 import UIKit
+import Charts
+
+import Foundation
+
+// MARK: - Week
+struct Week: Codable {
+    let the0, the1, the2, the3: Int
+    let the4, the5, the6: Int
+    let weather: String
+
+    enum CodingKeys: String, CodingKey {
+        case the0 = "0"
+        case the1 = "1"
+        case the2 = "2"
+        case the3 = "3"
+        case the4 = "4"
+        case the5 = "5"
+        case the6 = "6"
+        case weather
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bar_chart: BarChartView!
+    @IBOutlet weak var totalHitsLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        if let url = URL(string: "https://hayatopia.s3.amazonaws.com/weekData.json") {
+           URLSession.shared.dataTask(with: url) { data, response, error in
+              if let data = data {
+                  do {
+                    let data = try JSONDecoder().decode(Week.self, from: data)
+                    print(data.the3)
+                    
+                    
+                    let num = data.the0 + data.the1 + data.the2 + data.the3 + data.the4 + data.the5 + data.the6
+                    let weatherTemp = data.weather
+                    print(weatherTemp)
+                    self.updateLabel(hits: num, weather: weatherTemp)
+                    DispatchQueue.main.async {
+                     let week_data = ["1", "2", "3", "4", "5", "6", "7"]
+                        let vals = [data.the0, data.the1, data.the2, data.the3, data.the4, data.the5, data.the6]
+                     self.bar_chart.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                     self.bar_chart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+                     self.setChart_text(dataPoints: week_data, values: vals)
+                     
+                    }
+                    
 
+                  } catch let error {
+                     print(error)
+                  }
+               }
+           }.resume()
+        }
+    }
+    
+    
+    func updateLabel(hits: Int, weather: String){
+        DispatchQueue.main.async {
+            self.totalHitsLabel.text = "Total Hits this week: " + String(hits)
+            self.weatherLabel.text = "Average Outside Temp: " + String(weather) + "°C"
+        }
+    }
+    
+    
+    
+    func setChart_text(dataPoints: [String], values: [Int]) {
+        
+        bar_chart.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(values[i]))
+            dataEntries.append(dataEntry)
+            
+        }
+        
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Weekly Hits")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        bar_chart.leftAxis.enabled = false
+        bar_chart.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints)
+        bar_chart.xAxis.granularityEnabled = true
+        bar_chart.xAxis.labelPosition = .bottom
+        
+        chartDataSet.colors = [UIColor(red: 255/255, green: 126/255, blue: 0/255, alpha: 1)]
+        bar_chart.data = chartData
+    }
+    
+    @IBAction func refresh_button(_ sender: Any) {
+        self.bar_chart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+    }
+    
 
 }
 
